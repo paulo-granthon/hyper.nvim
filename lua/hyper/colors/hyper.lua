19429cb3ea95ceea48d1c33ec4a6d1c6bc1f126a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-09 23:05:50 -0300

    feat: dedicated `colors.hyper` module

---
+++ lua/hyper/colors/hyper.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+local base = {
+    black = '#000000',
+    white = '#ffffff',
+    red = '#ff0000',
+    green = '#33ff00',
+    yellow = '#ffff00',
+    blue = '#0066ff',
+    magenta = '#cc00ff',
+    light_gray = '#808080',
+    gray = '#666666',
+    dark_gray = '#3a3a3a',
+    darker_gray = '#333333',
+}
+
+M.colors = base
+
+function M.extend(overrides)
+    for k, v in pairs(overrides) do
+        overrides[k:lower()] = v
+    end
+    M.colors = vim.tbl_deep_extend("force", {}, base, overrides or {})
+end
+
+return M
