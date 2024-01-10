92644ef056890666abfcffff8b9d5eda834ea714
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-09 23:06:23 -0300

    fix: missing `const.error` module

---
+++ lua/hyper/const/error.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+---@type string
+M.import = 'Error importing `hyper.error` module.'
+
+---@type string
+M.err_types = 'Error importing `hyper.types.error` module inside `hyper.error`.'
+
+return M
