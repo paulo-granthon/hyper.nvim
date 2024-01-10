6d5448ffbb62c5391d23a32e6061c17a2af89c92
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-09 23:11:36 -0300

    feat: use `config` to define theme behavior

--- lua/hyper/init.lua
+++ lua/hyper/init.lua
@@ -5,14 +5,10 @@ if not ok then
     return vim.api.nvim_err_writeln(require('hyper.const.error').import)
 end
 
-local colors_ok, colors_or_err = pcall(require, 'hyper.colors')
-if not colors_ok then return error.throw(colors_or_err) end
+local config_ok, config_or_err = pcall(require, 'hyper.config')
+if not config_ok then return error.throw(config_or_err) end
 
-local colors = colors_or_err
-
-function M.load()
-    M.set_highlight()
-end
+local config = config_or_err
 
 function M.set_highlight()
     vim.cmd("hi clear")
@@ -24,9 +20,20 @@ function M.set_highlight()
     vim.o.termguicolors = true
     vim.g.colors_name = 'hyper'
 
-    for group, c in pairs(colors.get()) do
+    local engine_ok, engine_or_err = pcall(require, 'hyper.engine')
+    if not engine_ok then return error.throw(engine_or_err) end
+
+    local highlight_groups = engine_or_err.generate()
+
+    for group, c in pairs(highlight_groups) do
         vim.api.nvim_set_hl(0, group, c)
     end
 end
+
+function M.load()
+    M.set_highlight()
+end
+
+M.setup = config.setup
 
 return M
